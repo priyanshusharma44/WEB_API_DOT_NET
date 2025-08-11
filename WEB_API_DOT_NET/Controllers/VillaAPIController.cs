2f73bdf7a0560809fe955a967f89f5db7adebd33
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-08-11 21:20:11 +0545

    GETENDPOINT

---
+++ WEB_API_DOT_NET/Controllers/VillaAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_API_DOT_NET.Models;
+
+namespace WEB_API_DOT_NET.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController: ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa
+                {
+                    Id = 1,
+                    Name="RAM"
+                },
+                new Villa
+                {
+                    Id=2,
+                    Name="SHYAM"
+                },
+                new Villa
+                {
+                    Id=3,
+                    Name="HARI"
+                }
+
+
+            };
+        }
+    }
+}
+ 
---
+++ WEB_API_DOT_NET/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_API_DOT_NET.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
