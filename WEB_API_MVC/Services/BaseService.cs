61cae6530f1eb9bd3a160f8c67b8b2ef0f9dc433
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-09-20 21:09:38 +0545

    APIWEBMVC

---
+++ WEB_API_MVC/Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using WEB_API_MVC.Models;
+using WEB_API_MVC.Services.IServices;
+
+namespace WEB_API_MVC.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService() { 
+        this.responseModel = new APIResponse();
+        this.httpClient = httpClient;
+        }
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+            }
+        }
+    }
+}
---
+++ WEB_API_MVC/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using WEB_API_MVC.Models;
+
+namespace WEB_API_MVC.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
