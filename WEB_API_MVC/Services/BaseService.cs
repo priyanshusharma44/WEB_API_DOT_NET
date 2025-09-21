8447cdb5dc0b8f0b4334b2c6d53f4b61fbcb1a8b
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-09-21 13:21:34 +0545

    ConsumeAPIbaseService

--- WEB_API_MVC/Services/BaseService.cs
+++ WEB_API_MVC/Services/BaseService.cs
@@ -1,4 +1,7 @@
-﻿using WEB_API_MVC.Models;
+﻿using Newtonsoft.Json;
+using System.Text;
+using Villa_Utility;
+using WEB_API_MVC.Models;
 using WEB_API_MVC.Services.IServices;
 
 namespace WEB_API_MVC.Services
@@ -7,11 +10,14 @@ namespace WEB_API_MVC.Services
     {
         public APIResponse responseModel { get; set; }
         public IHttpClientFactory httpClient { get; set; }
-        public BaseService() { 
-        this.responseModel = new APIResponse();
-        this.httpClient = httpClient;
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new APIResponse();
+            this.httpClient = httpClient;
         }
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
             try
             {
@@ -19,6 +25,52 @@ namespace WEB_API_MVC.Services
                 HttpRequestMessage message = new HttpRequestMessage();
                 message.Headers.Add("Accept", "application/json");
                 message.RequestUri = new Uri(apiRequest.Url);
+
+                // Decide request type
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                // Add body only if data is provided
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(
+                        JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+                }
+
+                // Send request
+                HttpResponseMessage apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorsMessage = new List<string> { ex.Message },
+                    IsSuccess = false
+                };
+
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
             }
         }
     }
