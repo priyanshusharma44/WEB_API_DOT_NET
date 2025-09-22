a30339c14672f8de78242230ff761d6933c4a480
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-09-22 22:36:34 +0545

    FETCHED

---
+++ WEB_API_MVC/Controllers/VillaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_API_MVC.Models;
+using WEB_API_MVC.Models.DTO;
+using WEB_API_MVC.Services.IServices;
+
+namespace WEB_API_MVC.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        public VillaController(IVillaService villaService)
+        {
+            _villaService = villaService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if(response! == null)
+            {
+                var villas = response.Result;
+                return View(villas);
+            }
+            return View(new List<VillaDTO>());
+
+        }
+    }
+}
---
+++ WEB_API_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WEB_API_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
